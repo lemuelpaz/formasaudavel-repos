7108fa3bd6101815d90badd44787b132f2a9f8d2
Joel Andrade <joel.a.rocha@hotmail.com>
2024-02-02 20:31:55 -0300

    criando campo image em profissional

---
+++ Migrations/20240202233108_AddImagemProfissional.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImagemProfissional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Medico",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Medico");
+        }
+    }
+}
